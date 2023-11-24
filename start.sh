d41a052379e1e15fba9716be5b6cdb553b61bd10
Herokuown1 <139004262+Herokuown1@users.noreply.github.com>
2023-11-24 17:28:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Herokuown1.git /DQ-the-file-donor
+  git clone https://github.com/Herokuown1/DQ-the-file-donor.git /DQTheFileDonorBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-the-file-donor
+  git clone $UPSTREAM_REPO /DQTheFileDonorBot
 fi
 cd /DQ-the-file-donor
 pip3 install -U -r requirements.txt
